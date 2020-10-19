03211822909b92eea10c0e981fb5ad49ef0afb28
Chubareva <33682126+Chubareva@users.noreply.github.com>
2020-10-19 16:28:25 +0300

    refactor: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = '';
+    if (n % 10 == 0) {
+        n = n / 10;
+    }
+    let arr = n.toString();
+    if (arr[0] === '-') {
+
+        for (let i = 1; i < arr.length; i++) {
+            res = arr[i] + res;
+        }
+    } else {
+        for (let i = 0; i < arr.length; i++) {
+            res = arr[i] + res;
+        }
+    }
+    return res;
 }
